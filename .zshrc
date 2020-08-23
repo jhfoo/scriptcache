e5381ff57ebf9cb0f97795f058f44a50b19837b0
Foo JH <jhfoo@hansel.kungfoo.info>
2020-08-23 14:17:16 +0800

    NEW: .zshrc installer

---
+++ .zshrc
@@ -0,0 +1,6 @@
+alias jill='ssh jill'
+alias jack='ssh jack'
+alias hill='ssh 192.168.1.6'
+alias ntfs='sudo ntfs-3g /dev/da1s1 /ntfs -o rw,uid=1001,gid=1001'
+alias grumpy='ssh pi@192.168.50.21'
+alias siocage='sudo iocage $@'
--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 # meta installer
 echo Installing tmux scripts >&1
 curl https://raw.githubusercontent.com/jhfoo/scriptcache/master/tmux/install.sh | zsh
+echo Installing .zshrc >&1
+cd ~
+curl -O https://raw.githubusercontent.com/jhfoo/scriptcache/master/.zshrc
+cat .zshrc | zsh
